9b3acfa80626c744ffbf7e3a48b2d1fab0c26a08
Sophia Kim <sophiakim@8thlight.com>
2022-09-25 14:08:29 -0500

    update repo-detail#fetchRepo() method to implement switchMap instead of nesting subscriptions

--- src/app/repo-detail/repo-detail.component.ts
+++ src/app/repo-detail/repo-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable, switchMap, filter } from 'rxjs';
 import { RepoService } from '../repo.service';
 import { Repository } from '../repository';
 
@@ -9,8 +10,8 @@ import { Repository } from '../repository';
   styleUrls: ['./repo-detail.component.css']
 })
 export class RepoDetailComponent implements OnInit {
-  @Input() repo?: Repository;
-  name?: string;
+  repo?: Repository;
+  name?: Observable<string>;
   repoDetails: string[][] = [];
   repoFound?: boolean;
   
@@ -24,26 +25,26 @@ export class RepoDetailComponent implements OnInit {
   }
 
   fetchRepo(): void {
-    this.route.params.subscribe((params) => {
-      this.name = params['name'];
-    });
-    if (this.name) {
-      this.repoService.getRepo(this.name)
-        .subscribe(repo => {
-          if (repo?.name) {
-            this.repo = repo;
-            this.mapDetails();
-            this.repoFound = true;
-          }
-        });
-    }
+    this.route.params
+      .pipe(
+        filter(params => params['name']),
+        switchMap(params => {
+          this.name = params['name'];
+          return this.repoService.getRepo(params['name']);
+        })
+      ).subscribe(repo => {
+        if (repo?.name) {
+          this.repo = repo;
+          this.mapDetails();
+          this.repoFound = true;
+        }
+      })
   }
 
   mapDetails(): void {
     if (this.repo) {
       let details = Object.entries(this.repo).map(([k, v]) => {
         let key = k === "openPRs" ? "Open PRs" : k.replace(/([A-Z])/g, " $1");
-
         let value = !v ? "not available" : v;
 
         return [key, value];
